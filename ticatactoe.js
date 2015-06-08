db288c440018395815ce33d66c230694e06803b1
Andres Montenegro Montenegro <a.r.montenegro@gmail.com>
2015-06-08 15:37:08 -0400

    restarted tictactoe

---
+++ ticatactoe.js
@@ -0,0 +1,45 @@
+prompt("Player 1 Enter Your Name");
+prompt("Player 2 Enter Your Name")
+var player = 1;
+var player =2;
+
+}
+
+function winner(){
+	}
+
+//var player = [Player1, Player2]
+//var board = [Diagonal, horrizontal]
+
+//var checkWinner =  function();
+
+//var board = verticle 
+//
+//var board2 = horrizontal
+//
+//	if moves = 9 
+//	then  draw
+
+//function to decide player
+
+
+
+//alert("Player 1 X wins");
+
+//alert("Player 2 O wins");
+
+
+
+
+
+
+// 2 arrays
+// One for columns 
+//one for rows
+// Need 4 functions
+// need 2 variables
+// set event listeners to mark off X's and O's after making board element.
+// var for who is the player
+// set limit to moves
+// later on use 9 if statements to set up AI 
+//reset button
