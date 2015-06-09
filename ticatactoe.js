05a7df5f73c1a67022e01b7c521803b30945ec10
Andres Montenegro Montenegro <a.r.montenegro@gmail.com>
2015-06-09 02:48:08 -0400

    lacking restart function

--- ticatactoe.js
+++ ticatactoe.js
@@ -1,12 +1,89 @@
-prompt("Player 1 Enter Your Name");
-prompt("Player 2 Enter Your Name")
-var player = 1;
-var player =2;
+//couldnt figure out how to send prompt message to player 1x and player 2 y field
+//prompt("P1 Enter Your Name You Will be x");
+//prompt("P2 Enter Your Name You Will be o")
 
+var player = 1;
+function clickBt(bt){
+if(player == 1){
+//accepts x entries until o's turn
+	document.getElementById(bt).value = "x";
+	document.getElementById(bt).disabled = "null";
+	player -= 1;
+	winner();
+//accepts O entries until x's turn
+}else {
+	document.getElementById(bt).value = "o";
+	document.getElementById(bt).disabled = "null";
+	player += 1;
+	winner();
+}
+}
+//None of this is DRY brute force method of verifying x wins
+function winner(){
+	if(
+		document.getElementById("bt1").value == "x" &&
+		document.getElementById("bt2").value == "x" &&
+		document.getElementById("bt3").value == "x" ||
+		document.getElementById("bt4").value == "x" &&
+		document.getElementById("bt5").value == "x" &&
+		document.getElementById("bt6").value == "x" ||
+		document.getElementById("bt7").value == "x" &&
+		document.getElementById("bt8").value == "x" &&
+		document.getElementById("bt9").value == "x" ||
+		document.getElementById("bt1").value == "x" &&
+		document.getElementById("bt4").value == "x" &&
+		document.getElementById("bt7").value == "x" ||
+		document.getElementById("bt2").value == "x" &&
+		document.getElementById("bt5").value == "x" &&
+		document.getElementById("bt8").value == "x" ||
+		document.getElementById("bt3").value == "x" &&
+		document.getElementById("bt6").value == "x" &&
+		document.getElementById("bt9").value == "x" ||
+		document.getElementById("bt1").value == "x" &&
+		document.getElementById("bt5").value == "x" &&
+		document.getElementById("bt9").value == "x" ||
+		document.getElementById("bt3").value == "x" &&
+		document.getElementById("bt5").value == "x" &&
+		document.getElementById("bt7").value == "x"
+){
+		alert("X Player Wins!");
+		
+
+	}else if(
+		//when you dont know how to make a proper array .
+		//you brute force your way through it.
+		document.getElementById("bt1").value == "o" &&
+		document.getElementById("bt2").value == "o" &&
+		document.getElementById("bt3").value == "o" ||
+		document.getElementById("bt4").value == "o" &&
+		document.getElementById("bt5").value == "o" &&
+		document.getElementById("bt6").value == "o" ||
+		document.getElementById("bt7").value == "o" &&
+		document.getElementById("bt8").value == "o" &&
+		document.getElementById("bt9").value == "o" ||
+		document.getElementById("bt1").value == "o" &&
+		document.getElementById("bt4").value == "o" &&
+		document.getElementById("bt7").value == "o" ||
+		document.getElementById("bt2").value == "o" &&
+		document.getElementById("bt5").value == "o" &&
+		document.getElementById("bt8").value == "o" ||
+		document.getElementById("bt3").value == "o" &&
+		document.getElementById("bt6").value == "o" &&
+		document.getElementById("bt9").value == "o" ||
+		document.getElementById("bt1").value == "o" &&
+		document.getElementById("bt5").value == "o" &&
+		document.getElementById("bt9").value == "o" ||
+		document.getElementById("bt3").value == "o" &&
+		document.getElementById("bt5").value == "o" &&
+		document.getElementById("bt7").value == "o"
+){
+		alert("O Player Wins!");
+		
+	}
 }
 
-function winner(){
-	}
+
+
 
 //var player = [Player1, Player2]
 //var board = [Diagonal, horrizontal]
